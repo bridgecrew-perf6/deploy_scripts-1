19c738d76e532f54e419b0ca80df3a6051a2046c
hjabdoul <102533892+hjabdoul@users.noreply.github.com>
2022-04-01 16:11:54 +0200

    Create prepod_package.sh

---
+++ prepod_package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update -y
+
+sudo apt install vim -y
+vim --version
+
+sudo apt-get update -y
+sudo apt-get install strace -y
+
+sudo apt-get update
+sudo apt-get install ltrace
