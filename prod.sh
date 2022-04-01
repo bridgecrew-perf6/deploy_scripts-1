7bb3b9f4746b48157a7381ac893e659824aedbde
hjabdoul <102533892+hjabdoul@users.noreply.github.com>
2022-04-01 16:55:23 +0200

    Update prod.sh

--- prod.sh
+++ prod.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo apt-get update -y
+apt-get update -y
 
-sudo apt install vim -y
+apt install vim -y
 vim --version
 
-sudo apt-get update -y
-sudo apt-get install strace -y
+apt-get update -y
+apt-get install strace -y
 
-sudo apt-get update
-sudo apt-get install ltrace
+apt-get update
+apt-get install ltrace -y
